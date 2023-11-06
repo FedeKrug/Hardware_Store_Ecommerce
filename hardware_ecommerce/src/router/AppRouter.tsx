0541366320e48dc2d85e391657be99636e820038
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-11-06 18:18:31 -0300

    React-Router-Dom and AppRouter.tsx

---
+++ hardware_ecommerce/src/router/AppRouter.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
+
+export const AppRouter = () => {
+    return <>
+        <Routes>
+
+            {/* <Route path='/' element={<HomePage />} />
+            <Route path='/sobre-nosotros' element={<AboutPage />} />
+            <Route path='/contacto' element={<ContactPage />} />
+            <Route path='/donantes-y-colaboradores' element={<DonorsPage />} />
+            <Route path='/nuestras-causas' element={<DonorsPage />} />
+            <Route path='/shop-solidario' element={<ShopPage />} />
+            <Route path='/voluntarios' element={<VolunteersPage />} />
+            <Route path='/eventos' element={<EventsPage />} />
+            <Route path='/historias-de-vida' element={<StoriesPage />} />
+            <Route path='/blog' element={<BlogPage />} />
+            <Route path='/galeria' element={<GalleryPage />} />
+            <Route path='/preguntas-frecuentes' element={<FAQPage />} />
+            <Route path='/nuestras-causas' element={<FAQPage />} />
+
+
+
+            <Route path='/*' element={<Error404Page />} /> */}
+        </Routes>
+    </>
+
+}
